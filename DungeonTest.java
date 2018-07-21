802e581d7c90fce0a56f3e66a0dc5028756d86a8
Kunrenfi <a1243729005@qq.com>
2018-07-21 11:23:50 +0800

    git测试

---
+++ DungeonTest.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Administrator on 2016/7/13.
+ */
+import java.io.*;
+public class DungeonTest {
+    public static void main(String [] args){
+        DungeonGame d = new DungeonGame();
+        System.out.println(d.getX()+d.getY()+d.getZ());
+        try {
+            FileOutputStream fos = new FileOutputStream("dg.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(d);
+            oos.close();
+            FileInputStream fis = new FileInputStream("dg.ser");
+            ObjectInputStream ois =new ObjectInputStream(fis);
+            d =(DungeonGame)  ois.readObject();
+            ois.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(d.getX()+d.getY()+d.getZ());
+    }
+}
+//git测试
+
+class DungeonGame implements Serializable{
+    public int x =3;
+    transient  long y =4;
+    private short z=5;
+    int getX(){
+        return x;
+    }
+    long getY(){
+        return y;
+    }
+    short getZ(){
+        return z;
+    }
+}
